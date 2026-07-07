c813e8ca9ee615857f96d05c1aa11aa199faec16
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-07-08 01:54:18 +0300

    Leetcode 613- shortest distance in a line

--- database/613-Shortest-distance-in-a-line.sql
+++ database/613-Shortest-distance-in-a-line.sql
@@ -3,7 +3,7 @@
 -- Link: https://leetcode.com/problems/shortest-distance-in-a-line/description/
 -- Approach: We need to find the shortest distance between any two points on a line. 
 -- This can be done by calculating the absolute difference between the x-coordinates of all pairs of points and selecting the minimum.
--- We join tables on each non equal value to its O(n^2) time complexity
+-- We join tables on each non equal value to its O(n^2) time complexity.
 
 
 select MIN(ABS(p1.x - p2.x))  as shortest
