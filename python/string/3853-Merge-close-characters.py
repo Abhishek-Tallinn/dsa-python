dfbbb243a57fbe346368a7173ee7df5a9c2e4227
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-08-14 16:25:25 +0300

    Leetcode 3853- Merge close characters

---
+++ python/string/3853-Merge-close-characters.py
@@ -0,0 +1,21 @@
+# Problem: Leetcode 3853 - Merge close characters
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/merge-close-characters/description/
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Approach - we record the last seen position of each character in the hashmap and keep looking at each character and check if its current position is whithin
+# k range of the previous seen index. If yes then we skip else we update the last position and add it to result
+
+class Solution:
+    def mergeCharacters(self, s: str, k: int) -> str:
+        result = []
+        last_pos = {}
+
+        for c in s:
+            if c in last_pos and len(result) - last_pos[c] <= k:
+                continue
+            last_pos[c] = len(result)
+            result.append(c)
+
+        return "".join(result)
+                
