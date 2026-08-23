d4681c9fb29d939b9acd20b394ea44bf1d5e3871
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-08-23 20:29:29 +0300

    Leetcode 94-Binary tree inorder traversal

---
+++ python/tree/94-Binary-tree-inorder-traversal.py
@@ -0,0 +1,28 @@
+# Problem: Leetcode 94 - Binary tree inorder traversal
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+# Time Complexity: O(n) as we iterate through the array elements
+# Space Complexity: O(n) as we use a stack to push and pop elements
+# Approach: We do the usual inorder traversal where we traverse left and then append the value 
+# and then traverse right node in each recursive call
+
+from typing import Optional
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        def traverse(root):
+            if not root:
+                return 
+            traverse(root.left)
+            ans.append(root.val)
+            traverse(root.right)
+        traverse(root)
+        return ans
