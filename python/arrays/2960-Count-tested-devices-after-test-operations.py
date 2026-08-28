f12c8633407c1da76594ac2ef89cb50772912e56
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-08-28 12:25:12 +0300

    Leetcode 2960-Count tested devices after test operations

---
+++ python/arrays/2960-Count-tested-devices-after-test-operations.py
@@ -0,0 +1,20 @@
+#Problem: Leetcode 2960 - Count tested devices after test operations
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/count-tested-devices-after-test-operations/description/
+# Time Complexity: O(n) - as we use sliding window
+# Space Complexity: O(n) as we use a hashmap
+# Approach: We keep a decrement counter which increases everytime we are able to decrement a battery by one and each subsequent battery
+# is checked against this decrement counter. If after subtracting decrement counter its still >0 then we know 
+# that this battery will survive and we can increment our counter.
+
+from typing import List
+
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        dec=0
+        cnt=0
+        for i in range(len(batteryPercentages)):
+            if batteryPercentages[i]-dec >0:
+                cnt+=1
+                dec+=1
+        return cnt
