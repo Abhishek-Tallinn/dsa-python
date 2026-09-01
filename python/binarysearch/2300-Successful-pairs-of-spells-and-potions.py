498c0ba2bce6ac86d1a90b68f007bf1411188c81
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-09-01 11:18:08 +0300

    Leetcode 2300-Successful pairs of spells and potions

--- python/binarysearch/2300-Successful-pairs-of-spells-and-potions.py
+++ python/binarysearch/2300-Successful-pairs-of-spells-and-potions.py
@@ -3,11 +3,10 @@
 # Link: https://leetcode.com/problems/successful-pairs-of-spells-and-potions/description/
 # Time Complexity: O(n log n) as we do a binary search over the array
 # Space Complexity: O(1) as no extra data structure is added
-# Approach: We convert the question to binary search on solution space. we sort the price array and then we say that
-# the max diff that will allow us to select a basket of k things from the price array is between 0 and the difference between first and last array.
-# based on this idea we return a binary search on solution space and keep recording the first true value of mid which is feasible.
-# at the end we return this feasible amount. 
-# appraoch2: we can also do the inverted template where we record the
+# Approach: We sort potions and do a binary search to find the first index from where spell*potion value >=success. 
+# then we update the answer array with the remanining number of potions value by doing len(potions)-left as left pointer will find the first
+# index from where the value of spell*potion[i] becomes > success and since the relationship is monotonic
+# we can just do ans[j] = len(spotions) - left
 
 from typing import List
 
