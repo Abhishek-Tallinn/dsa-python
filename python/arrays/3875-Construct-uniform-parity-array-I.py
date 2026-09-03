5010a3acff038cd1e14ed00b61b2f15619ada996
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-09-03 12:11:53 +0300

    Leetcode 3875-Construct uniform parity array II

--- python/arrays/3875-Construct-uniform-parity-array-I.py
+++ python/arrays/3875-Construct-uniform-parity-array-I.py
@@ -1,3 +1,12 @@
+# Problem: Leetcode 3875 - Construct uniform parity array I
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+# Approach: If array is all odd or even then its already True. and if it has both odd and even elements
+# then we can always take any even element and subtract any odd element from it and make it odd.
+# so it will always be possible to sort the array to all odd elements in this case.
+
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
         return True
