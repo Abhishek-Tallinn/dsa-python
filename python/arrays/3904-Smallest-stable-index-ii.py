fdd733caa3ffb7ed4531d844dd30a7677b561f3f
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-09-04 10:57:23 +0300

    Leetcode 3904-Smallest stable index II

---
+++ python/arrays/3904-Smallest-stable-index-ii.py
@@ -0,0 +1,23 @@
+# Problem: Leetcode 3904 - Smallest Stable Index II
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/smallest-stable-index-ii/description/
+# Time Complexity: O(n) 
+# Space Complexity: O(n) as we use prefix and suffix array
+# Approach: We create prefix and suffix array in one loop and then use it to check if difference at any index is <= k.
+# if yes then we immediately take that index and break as we need the smallest index and we return -1.
+# precalculation of prefix and suffix sums helps the loop up in o(1)
+
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        smallest_stable_index = -1
+        prefix_mx =  [nums[0]]
+        suffix_mn = [nums[-1]]
+        for i in range(1,len(nums)):
+            prefix_mx.append(max(prefix_mx[-1],nums[i]))
+            suffix_mn.append(min(suffix_mn[-1],nums[len(nums)-i-1]))
+        suffix_mn = suffix_mn[::-1]
+        for i in range(len(nums)):
+            if prefix_mx[i] - suffix_mn[i] <= k:
+                smallest_stable_index = i
+                break
+        return smallest_stable_index 
