b43341ea6e0639ce869d13c4a93fc8d070f81388
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-09-06 01:47:53 +0300

    Leetcode 1052-Grumpy bookstore owner

---
+++ python/arrays/1052-Grumpy-bookstore-owner.py
@@ -0,0 +1,28 @@
+# Problem: Leetcode 1052 - Grumpy bookstore owner
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/grumpy-bookstore-owner/description/
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Approach: We calculate the total already safisfied customers and then use prefix sum where the prefix sum
+# is based on those values when is owner is grumpy and excludes values when he is non grumpy so that the window 
+# only has gains in it the effective gain. then we just iterate over the window and check the mx gain possible
+# then we return the original satisfied customers + max gain 
+
+from typing import List
+
+
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        #satisfied = sum(c for c,g in zip(customers,grumpy) if not g)
+        satisfied = 0
+        for i in range(len(customers)):
+            if not grumpy[i]:
+                satisfied+=customers[i]
+        prefix = [0]*(len(customers)+1)
+        for i in range(len(customers)):
+            prefix[i+1] = prefix[i] + (customers[i] if grumpy[i] else 0)
+    
+        mx = 0
+        for i in range(len(grumpy)-minutes+1):
+            mx  = max(mx, prefix[i+minutes] - prefix[i]) 
+        return satisfied + mx
