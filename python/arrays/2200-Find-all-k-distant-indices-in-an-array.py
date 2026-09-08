8dfbe23f7ab517360d04a8054646bee43a17151a
Abhishek Choudhary <choudharyabhishek9876@gmail.com>
2026-09-08 21:44:45 +0300

    Leetcode 2200-Find all k distinct indices in an array

--- python/arrays/2200-Find-all-k-distant-indices-in-an-array.py
+++ python/arrays/2200-Find-all-k-distant-indices-in-an-array.py
@@ -1,3 +1,12 @@
+# Problem: Leetcode 2200 - Keep multiplying found values by 2
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/description/
+# Time Complexity: O(n)
+# Space Complexity: O(1) 
+# Approach: We simply track the indices where key exists first. We can add them in hashmap too but the input size is small
+# so look up in a list which is O(n) will also work. Then we loop the array and check in nested loop if the current index has any key indices 
+# for which the absolute value of diffence is less than 1. If yes we append and break immediately to check next index.
+
 from typing import List
 
 class Solution:
